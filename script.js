9b07d665ed21effcb51cddd67114c13b8072e69b
Mukhtar <example@gmail.com>
2020-01-25 19:56:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict'
+//let a =  5;
+//console.log(a);
+
+//let arr = ['apple.png', 'orange.png']
+//console.log(arr[0])
+//alert("Hello world!")
+
+// let answer = confirm("Are u here?");
+// console.log(answer);
+
+//let answer = prompt("Есит ли вам 18?", "Да");
+//console.log(typeof (arr));
+
+function test () {
+    let b = document.getElementById('test').value;
+    console.log(b);
+}
+
+
+
